3b0aab6628e74361cb7e334634973c371724d9c8
yvzn <mail@ludeo.net>
2018-01-25 14:16:29 +0100

    Moved to webpack to bundle stuff (wip)

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        conductor: './src/conductor/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        host: '0.0.0.0'
+    },
+    plugins: [
+        new CleanWebpackPlugin('dist'),
+        new CopyWebpackPlugin([
+            { from: 'src/widgets' }
+        ]),
+        new HtmlWebpackPlugin({
+            title: 'Wallboard'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
